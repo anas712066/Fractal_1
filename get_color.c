0758b7aed255edba641f67b91043720d4b41cfa3
Muhammad Anas <anasabid607@gmail.com>
2025-03-08 20:23:51 +0100

    fractal

--- get_color.c
+++ get_color.c
@@ -6,28 +6,25 @@
 /*   By: mumajeed <mumajeed@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 22:25:11 by mumajeed          #+#    #+#             */
-/*   Updated: 2025/02/22 19:50:17 by mumajeed         ###   ########.fr       */
+/*   Updated: 2025/03/08 20:09:16 by mumajeed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int get_color(int iteration, int max_iteration, t_fractal *fractal)
+int	get_color(int iteration, int max_iteration, t_fractal *fractal)
 {
-    double  t;
-    t_color	color;
+	double	t;
+	t_color	color;
 
-    if (iteration >= max_iteration)
-        return (BLACK);
-    t = (double)iteration / (double)max_iteration;
-    color.red = (int)(9 * (1 - t) * t * t * t * 255);
-    color.green = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
-    color.blue = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
-
-    // Apply color shift
-    color.red = (color.red + fractal->color_shift) % 256;
-    color.green = (color.green + fractal->color_shift) % 256;
-    color.blue = (color.blue + fractal->color_shift) % 256;
-
-    return ((color.red << 16) | (color.green << 8) | color.blue);
+	if (iteration >= max_iteration)
+		return (BLACK);
+	t = (double)iteration / (double)max_iteration;
+	color.red = (int)(9 * (1 - t) * t * t * t * 255);
+	color.green = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
+	color.blue = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
+	color.red = (color.red + fractal->color_shift) % 256;
+	color.green = (color.green + fractal->color_shift) % 256;
+	color.blue = (color.blue + fractal->color_shift) % 256;
+	return ((color.red << 16) | (color.green << 8) | color.blue);
 }
--- main.c
+++ main.c
@@ -6,80 +6,17 @@
 /*   By: mumajeed <mumajeed@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 13:47:49 by mumajeed          #+#    #+#             */
-/*   Updated: 2025/02/25 21:33:30 by mumajeed         ###   ########.fr       */
+/*   Updated: 2025/03/08 20:23:30 by mumajeed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-/*int	main(int ac, char **av)
-{
-	t_fractal	fractal;
-
-	if ((ac == 2 && !ft_strncmp(av[1], "mandelbrot", 10)) ||
-		(ac == 4 && !ft_strncmp(av[1], "julia", 5)))
-	{
-		fractal.name = av[1];
-		fractal.julia_x = atodbl(av[2]);
-		fractal.julia_y = atodbl(av[3]);
-		// Inicia la aplicación correctamente
-		fractal_init(&fractal);
-		fractal_render(&fractal);
-		mlx_loop(fractal.mlx_connection);
-	}
-	else
-	{
-		putstr_fd(ERROR_MESSAGE, STDERR_FILENO);
-		exit(EXIT_FAILURE);
-	}
-}*/
-/*int	main(int ac, char **av)
-{
-	t_fractal	fractal;
-
-	if ((ac == 2 && !ft_strncmp(av[1], "mandelbrot", 10)) ||
-		(ac == 4 && !ft_strncmp(av[1], "julia", 5)))
-	{
-		fractal.name = av[1];
-
-		Inicializar julia_x y julia_y solo si es julia 
-if (!ft_strncmp(av[1], "julia", 5))
-{
-    if (ac == 4)
-    {
-        fractal.julia_x = atodbl(av[2]);
-        fractal.julia_y = atodbl(av[3]);
-    }
-    else
-    {
-        fractal.julia_x = -0.7; // Valores por defecto
-        fractal.julia_y = 0.27015;
-    }
-}
-
-	else
-	{
-		fractal.julia_x = 0.0;
-		fractal.julia_y = 0.0;
-	}
-
-		fractal_init(&fractal);
-		fractal_render(&fractal);
-		if (fractal.mlx_connection && fractal.mlx_window)
-			mlx_loop(fractal.mlx_connection);
-	}
-	else
-	{
-		putstr_fd(ERROR_MESSAGE, STDERR_FILENO);
-		exit(EXIT_FAILURE);
-	}
-}*/
 #include "fractol.h"
 #include "minilibx-linux/mlx.h"
 
 static int	check_args(char *arg)
 {
 	size_t	i;
-	int	point_found;
+	int		point_found;
 
 	point_found = 0;
 	i = 0;
@@ -89,14 +26,14 @@ static int	check_args(char *arg)
 	while (arg[i] != '\0')
 	{
 		if (!ft_isdigit(arg[i]) && arg[i] != '.')
-		       return (0);
+			return (0);
 		if (arg[i] == '.')
 		{
 			if (point_found == 0)
 				point_found = 1;
 			else
 				return (0);
-		}		
+		}
 		i++;
 	}
 	return (1);
@@ -104,16 +41,16 @@ static int	check_args(char *arg)
 
 static void	init_julia(t_fractal *fractal, char **av)
 {
-    if (check_args(av[2]) == 1 && check_args(av[3]) == 1)
-    {
-        fractal->julia_x = atodbl(av[2]);
-        fractal->julia_y = atodbl(av[3]);
-	if (fractal->julia_x > 1 || fractal->julia_x < -1 || 
-			fractal->julia_y > 1 || fractal->julia_y < -1)
+	if (check_args(av[2]) == 1 && check_args(av[3]) == 1)
 	{
-		putstr_fd("Argument out of range: please give numbers between 1 and -1\n", 2);
-		exit(0);
-	}
+		fractal->julia_x = atodbl(av[2]);
+		fractal->julia_y = atodbl(av[3]);
+		if (fractal->julia_x > 1 || fractal->julia_x < -1
+			|| fractal->julia_y > 1 || fractal->julia_y < -1)
+		{
+			putstr_fd("Wrong input: put number between 1 and -1\n", 2);
+			exit(0);
+		}
     }
     else
     {
