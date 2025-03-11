835e99dfc5298b9529a931dcf6338b9095327a6f
Muhammad Anas <anasabid607@gmail.com>
2025-03-12 00:48:45 +0100

    fractal

--- get_color.c
+++ get_color.c
@@ -18,13 +18,18 @@ int	get_color(int iteration, int max_iteration, t_fractal *fractal)
 	t_color	color;
 
 	if (iteration >= max_iteration)
-		return (BLACK);
+		return (0x101010);
 	t = (double)iteration / (double)max_iteration;
-	color.red = (int)(9 * (1 - t) * t * t * t * 255);
-	color.green = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
-	color.blue = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
+	color.red = (int)(9 * (1 - t) * t * t * t * 255) + 50;
+	color.green = (int)(15 * (1 - t) * (1 - t) * t * t * 255) + 50;
+	color.blue = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255) + 50;
+    if (color.red < 50) color.red = 50;
+    if (color.green < 50) color.green = 50;
+    if (color.blue < 50) color.blue = 50;
 	color.red = (color.red + fractal->color_shift) % 256;
 	color.green = (color.green + fractal->color_shift) % 256;
 	color.blue = (color.blue + fractal->color_shift) % 256;
 	return ((color.red << 16) | (color.green << 8) | color.blue);
 }
+
+
